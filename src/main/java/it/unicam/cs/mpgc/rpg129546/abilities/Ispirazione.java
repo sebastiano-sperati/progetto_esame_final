0c54bd18952687e6936fb06d3e73bfdfa5202c86
sebastianosperati-spec <sebastianosperati@gmail.com>
2026-05-04 17:36:01 +0200

    inserito un sistema per far si che le abilità non possano essere usate in maniera impropria, quindi no attacchi agli alleati o agliuti ai nemici

--- src/main/java/it/unicam/cs/mpgc/rpg129546/abilities/Ispirazione.java
+++ src/main/java/it/unicam/cs/mpgc/rpg129546/abilities/Ispirazione.java
@@ -6,6 +6,10 @@ public class Ispirazione implements Action{
     private final int cost = 4;
     @Override
     public void execute(Entity source, Entity target) {
+        if(!source.isAlly(target)){
+            System.out.println("non puoi putenziare un nemico");
+            return;
+        }
         if(source.getAp() < cost) return;
         source.consumeAp(cost);
         target.setInspired(true);
